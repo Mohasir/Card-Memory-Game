ead70154aeaee615af6560b45f097da01d567711
Mohasir <mohasirfuentes12@gmail.com>
2020-04-16 15:13:02 -0600

    terminando funcionalidad con js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,3 @@
-
-
 let pathImages = ['img/cards/card-back.png',
     'img/cards/card-black-panther.png',
     'img/cards/card-black-widow.png',
@@ -11,16 +9,101 @@ let pathImages = ['img/cards/card-back.png',
     'img/cards/card-spiderman.png']
 
 
-let btnReplay = document.getElementById('btn-replay');
+let btnReplay = document.querySelectorAll('.btn-replay');
 let card = document.querySelectorAll('.card');
-let timer = document.getElementById('timer');
+let timer = document.querySelectorAll('.timer');
+let movements = document.querySelector('.movements');
 let popupWin = document.querySelector('.cont-popup')
 let scroll = document.querySelector('body')
-let images = [];
-let as = []
-let contador = 1
+let images = [], as = [];
+let pairs = 0, interval;
 
 window.addEventListener('load', () => {
+    startGame();
+});
+
+
+btnReplay.forEach((el) => {
+    el.addEventListener('click', () => {
+        startGame();
+        popupWin.style.display = 'none';
+        scroll.style.overflowY = 'scroll';
+        card.forEach((element) => element.src = pathImages[0]);
+    });
+})
+
+
+// agrega a cada elemento 'card' el evento 'click' para mostrar la carta
+card.forEach((element, i) => {
+    element.addEventListener('click', () => {
+        if (element.src.includes(pathImages[0])) {
+            if (as.length < 2) {
+                element.src = images[i];
+                anime({
+                    targets: element,
+                    scale: [0, 1],
+                    easing: 'easeOutExpo',
+                    duration: 500
+                });
+                as.push(element)
+                if (as.length == 2) {
+                    matching(as)  
+                }
+            }
+        }
+    });
+});
+
+
+// funcion retorna un 'array' con las rutas de las imagenes de forma aleatoria
+const getPathRandom = (n) => {
+    var array = []
+    while (array.length < n) {
+        var pathImg = pathImages[Math.ceil(Math.random() * n)];//guarda la ruta de una imagen de forma aleatoria
+        //verifica si en el array ya existe la ruta de la imagen
+        //si no exite agrega la ruta de la imagen al array
+        if (!array.includes(pathImg)) array[array.length] = pathImg;
+    }
+    return array = array.concat(array).sort(() => (Math.random() - 0.5)) //duplica el array y desordena los elementos
+}
+
+//funcion para comprobar si se econtro el par de la tarjeta
+const matching = (ar) => {
+    if (ar[0].src === ar[1].src) { //comprueba si el par son iguales
+        anime({
+            targets: ar,
+            scale: [1, 0],
+            easing: 'easeOutExpo',
+            duration: 500
+        });
+
+        //cuando se encuentra todos los pares se muestra el popup
+        if (pairs == 8) {
+            popupWin.style.display = 'flex';
+            scroll.style.overflowY = 'hidden';
+            clearInterval(interval)
+        }
+
+        pairs++
+        movements.textContent = parseInt(movements.textContent) + 1
+        as = [];
+    } else {   // si no son iguales se voltean las tarjetas
+        window.setTimeout(() => {
+            as.forEach((el) => el.src = pathImages[0])
+            anime({
+                targets: as,
+                scale: [0, 1],
+                easing: 'easeOutBounce',
+                duration: 500
+            });
+            as = [];
+            movements.textContent = parseInt(movements.textContent) + 1
+        }, 500);
+    }
+}
+
+//reinicia el juego y vuelve a colocar las tarjetas de forma aletoria
+const startGame = () => {
     anime({
         targets: '.dashboard-card .card',
         scale: [
@@ -29,122 +112,28 @@ window.addEventListener('load', () => {
         ],
         delay: anime.stagger(200, { grid: [4, 4], from: 'center' }),
     });
-    images = getPathRandom(8)
-});
-
-btnReplay.addEventListener('click', () => {
-    anime({
-        targets: '.dashboard-card .card',
-        scale: [
-            { value: .1, easing: 'easeOutSine', duration: 500 },
-            { value: 1, easing: 'easeInOutQuad', duration: 1200 }
-        ],
-        delay: anime.stagger(200, { grid: [4, 4], from: 'center' })
-    });
-
-    popupWin.style.display = 'none';
-    scroll.style.overflowY = 'scroll';
-    card.forEach((element) => element.src = pathImages[0]);
+    movements.textContent = 0;
+    pairs = 0;
     images = getPathRandom(8);
-});
-
-
-
-// funcion retorna el 'array' con las rutas de las imagenes de forma aleatoria
-const getPathRandom = (n) => {
-    var ar1 = [], ar2 = []
-    while ((ar1.length) < n & (ar2.length < n)) {
-        var pathImg = pathImages[Math.ceil(Math.random() * n)];//guarda la ruta de una imagen de forma aleatoria
-        //verifica si en el array ya existe la ruta de la imagen
-        //si no exite agrega la ruta de la imagen al array
-        if (!ar1.includes(pathImg)) ar1[ar1.length] = pathImg;
-        if (!ar2.includes(pathImg)) ar2[ar2.length] = pathImg;
-    }
-    console.log(1)
-    return array = ar1.concat(ar2).sort(() => (Math.random() - 0.5))
-
+    startTimer();
 }
 
-/* const getPathRandom = (n,array) => {
-    while (array.length < n) {
-        var pathImg = pathImages[Math.ceil(Math.random() * n)];
-        if (!array.includes(pathImg)) array[array.length] = pathImg;
-    }
-}  */
-
-
-// agrega a cada elemento 'card' el evento 'click' para mostrar la carta
-card.forEach((element, i) => {
-    element.addEventListener('click', () => {
-        if (element.src.includes(pathImages[0])) {
-            element.src = images[i];
-            anime({
-                targets: element,
-                scale: [0, 1],
-                easing: 'easeOutExpo',
-                duration: 500
-            });
-            as.push(element)
-            if (as.length == 2) {
-                if (as[0].src === as[1].src) {
-                    //as.forEach((el) => el.style.border = 'solid red 2px')
-                    anime({
-                        targets: as,
-                        scale: [1,0],
-                        easing: 'easeOutExpo',
-                        duration: 500
-                    });
-
-                    if(contador == 1){
-                        popupWin.style.display = 'flex';
-                        scroll.style.overflowY = 'hidden';
-                    }
-
-                    contador++
-                    as = [];
-                } else {
-                    window.setTimeout(() => {
-                        as.forEach((el) => el.src = pathImages[0])
-                        anime({
-                            targets: as,
-                            scale: [0, 1],
-                            easing: 'easeOutBounce',
-                            duration: 500
-                        });
-                        as = [];
-                    }, 500);
-                }
-
-            }
-        } else {
-            element.src = pathImages[0];
-            anime({
-                targets: element,
-                scale: [0, 1],
-                easing: 'easeOutBounce',
-                duration: 500
-            });
-        }
-    });
-});
-
-
-
-
+//funcion para iniciar el cronometro
 const startTimer = () => {
-    second = 50
-    minute = 0
-
-    var t = window.setInterval(() => {
-        if (second == 60) {
-            second = 0;
-            minute++;
+    var s = 0, m = 0
+    var second = '', minute = '';
+    clearInterval(interval)
+    interval = window.setInterval(() => {
+        if (s == 60) {
+            s = 0;
+            m++;
         }
-        timer.textContent = `${minute}:${second++}`;
+
+        (s < 10) ? second = `0${s++}` : second = `${s++}`;
+        (m < 10) ? minute = `0${m}` : minute = `${m}`;
+
+        timer.forEach((el) => el.textContent = `${minute}:${second}`)
+
     }, 1000);
 }
 
-
-/* function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-} */
